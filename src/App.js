fff199b7c9cf47c3725397ef98a4410d287e6bb0
Linda Lim <CookiesNCream@users.noreply.github.com>
2017-06-01 11:59:02 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,10 +16,13 @@ class App extends React.Component {
         <p className="App-intro">
           
           I can help you type less. All you need to do is to type &#35;, &#64;, &#60; and select the rest from a list.
-
-          Click <a href=" https://cookiesncream.github.io/autocomplete-hashtags/index.html">&#35;</a> for &#35; autocomplete, <a href="https://cookiesncream.github.io/autocomplete-mentions/index.html">&#64;</a> for &#64; autocomplete, <a href="https://cookiesncream.github.io/autocomplete-relations/index.html">&#60;&#62;</a> for &#60;&#62; autocomplete, and <a href="https://cookiesncream.github.io/autocomplete-mentions-pure-hashtags/index.html">autocomplete mentions and pure &#35; </a> for the hybrid.     
+     
         </p>
-    
+        <p>
+      
+          Click <a href=" https://cookiesncream.github.io/autocomplete-hashtags/index.html">&#35;</a> for &#35; autocomplete, <a href="https://cookiesncream.github.io/autocomplete-mentions/index.html">&#64;</a> for &#64; autocomplete, <a href="https://cookiesncream.github.io/autocomplete-relations/index.html">&#60;&#62;</a> for &#60;&#62; autocomplete, and <a href="https://cookiesncream.github.io/autocomplete-mentions-pure-hashtags/index.html">autocomplete mentions and pure &#35; </a> for the hybrid.
+      
+        </p>
       </div>
     );
   }
