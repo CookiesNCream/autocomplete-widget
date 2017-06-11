ab69461d117e6d0bf8eb1050e9bedd3761cda8be
CookiesNCream <CookiesNCream@users.noreply.github.com>
2017-06-11 18:07:49 +0800

    Add existing file

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends React.Component {
         </p>
          <p> 
           Click <a href="https://autocomplete-hashtags.firebaseapp.com">&#35;</a> for &#35; autocomplete, <a href="https://autocomplete-mentions.firebaseapp.com">&#64;</a> for &#64; autocomplete, <a href="https://autocomplete-relations.firebaseapp.com
-">&#60;&#62;</a> for &#60;&#62; autocomplete, and <a href="https://autocomplete-mentions-hashtags.firebaseapp.com">autocomplete mentions and pure &#35;</a> for the hybrid.     
+">&#60;&#62;</a> for &#60;&#62; autocomplete, <a href="https://autocomplete-mentions-hashtags.firebaseapp.com">autocomplete mentions and pure &#35;</a> for the hybrid, and <a href="https://autocomplete-ht-men-rel.firebaseapp.com/">&#35;, &#64;, &#60;&#62;</a> for &#35;, &#64;, &#60;&#62; autocomplete.     
 
         </p>
       </div>
